be7dd49a35240e19df486513602f5af9d74cbfc5
Aarão <115494573+Aarao3007@users.noreply.github.com>
2023-04-10 16:14:44 -0300

    Correção de bugs

---
+++ ListaDeTarefas/WindowsFormsPrincipal/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsPrincipal
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+        }
+    }
+}
