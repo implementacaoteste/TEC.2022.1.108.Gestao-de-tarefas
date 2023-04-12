f30874f186399b803b93093bf7b6ceb45156998e
guilherme27-03 <115494518+guilherme27-03@users.noreply.github.com>
2023-04-12 16:53:51 -0300

    Atualização

--- ListaDeTarefas/DAL/GrupoDAL.cs
+++ ListaDeTarefas/DAL/GrupoDAL.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using Model;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +10,39 @@ namespace DAL
 {
     public class GrupoDAL
     {
-        public void BuscarPorIdGrupo()
+        public Grupo BuscarPorIdGrupo(int id)
         {
+            Grupo _grupo = new Grupo();
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandText = @"SELECT FROM Grupo WHERE @IdGrupo = IdGrupo";
+                cmd.CommandType = System.Data.CommandType.Text;
 
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Parameters.AddWithValue("@IdGrupo",id);
+
+                cn.Open();
+
+                using (SqlDataReader rd = cmd.ExecuteReader())
+                {
+                    if (rd.Read())
+                    {
+                        _grupo.Id = Convert.ToInt32(rd["ID"]);
+                    }
+                }
+                return _grupo;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar buscar Id de Grupo no banco de dados", ex);
+            }
+            finally
+            {
+                cn.Close();
+            }
         }
         public void BuscarPorIdUsuario()
         {
@@ -18,7 +50,7 @@ namespace DAL
         }
         public void BuscarPorTodosGrupos()
         {
-
+              
         }
         public void BuscarPorTitulo()
         {
@@ -28,9 +60,27 @@ namespace DAL
         {
 
         }
-        public void AdicionarGrupo()
+        public void AdicionarGrupo(Grupo _grupo)
         {
-
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
+            {
+                SqlCommand cmd = cn.CreateCommand();
+                cmd.CommandText = @"INSERT INTO Grupo(Titulo) Values(@Titulo)";
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Parameters.AddWithValue("@Titulo", _grupo.Titulo);
+                cmd.Connection = cn;
+                cn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Ocorreu um erro ao inserir um Grupo no banco de dados", ex);
+            }
+            finally
+            {
+                cn.Close();
+            }
         }
         public void AlterarGrupo()
         {
