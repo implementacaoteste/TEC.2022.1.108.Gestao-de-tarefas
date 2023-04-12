451f431a5e98356f923722f2c918589eed400320
Aarão <115494573+Aarao3007@users.noreply.github.com>
2023-04-12 15:26:25 -0300

    Conexão com banco
    Código que executa um comando armazenado localmente na máquina e conecta o projeto com o banco

---
+++ ListaDeTarefas/DAL/Conexao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public static class Conexao
+    {
+        private static string stringDeConexao;
+        public static string StringDeConexao
+        {
+            get
+            {
+                if (String.IsNullOrEmpty(stringDeConexao))
+                    if (File.Exists("C:\\Configuracoes\\conexao.config"))
+                        stringDeConexao = File.ReadAllText("C:\\Configuracoes\\conexao.config");
+                    else throw new Exception("Você precisa criar à conexão com o banco de dados");
+                return stringDeConexao;
+            }
+        }
+    }
+}
--- ListaDeTarefas/WindowsFormsPrincipal/Program.cs
+++ ListaDeTarefas/WindowsFormsPrincipal/Program.cs
@@ -15,7 +15,7 @@ namespace WindowsFormsPrincipal
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-
+            Application.Run(new FormTelaPrincipal());
         }
     }
 }
