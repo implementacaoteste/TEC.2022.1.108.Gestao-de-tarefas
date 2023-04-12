1fd0a3d509e931a4f9008eef746ce2cda082cd71
Aarão <115494573+Aarao3007@users.noreply.github.com>
2023-04-12 14:50:16 -0300

    FormCadastroEtapa
    Criado a tela de cadastro de etapa

---
+++ ListaDeTarefas/WindowsFormsPrincipal/FormCadastroEtapa.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace WindowsFormsPrincipal
+{
+    partial class FormCadastroEtapa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBoxNomeEtapa = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonSalvarEtapa = new System.Windows.Forms.Button();
+            this.buttonCancelarEtapa = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(606, 80);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.DarkGray;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 80);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(107, 300);
+            this.panel2.TabIndex = 1;
+            // 
+            // textBoxNomeEtapa
+            // 
+            this.textBoxNomeEtapa.Location = new System.Drawing.Point(213, 214);
+            this.textBoxNomeEtapa.Name = "textBoxNomeEtapa";
+            this.textBoxNomeEtapa.Size = new System.Drawing.Size(283, 22);
+            this.textBoxNomeEtapa.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(213, 145);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(283, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(213, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nome da Etapa";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(213, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Responsável";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // buttonSalvarEtapa
+            // 
+            this.buttonSalvarEtapa.Location = new System.Drawing.Point(267, 295);
+            this.buttonSalvarEtapa.Name = "buttonSalvarEtapa";
+            this.buttonSalvarEtapa.Size = new System.Drawing.Size(75, 23);
+            this.buttonSalvarEtapa.TabIndex = 5;
+            this.buttonSalvarEtapa.Text = "Salvar";
+            this.buttonSalvarEtapa.UseVisualStyleBackColor = true;
+            // 
+            // buttonCancelarEtapa
+            // 
+            this.buttonCancelarEtapa.Location = new System.Drawing.Point(348, 295);
+            this.buttonCancelarEtapa.Name = "buttonCancelarEtapa";
+            this.buttonCancelarEtapa.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancelarEtapa.TabIndex = 5;
+            this.buttonCancelarEtapa.Text = "Cancelar";
+            this.buttonCancelarEtapa.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(277, 54);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Nova Etapa";
+            // 
+            // FormCadastroEtapa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(606, 380);
+            this.Controls.Add(this.buttonCancelarEtapa);
+            this.Controls.Add(this.buttonSalvarEtapa);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxNomeEtapa);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
+            this.Name = "FormCadastroEtapa";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro de etapa";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBoxNomeEtapa;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonSalvarEtapa;
+        private System.Windows.Forms.Button buttonCancelarEtapa;
+        private System.Windows.Forms.Label label3;
+    }
+}
