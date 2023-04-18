e20a49ca51739a57bdd01f576e86351555b7dda4
André Carlos Heringer <70924249+andrejr105@users.noreply.github.com>
2023-04-18 14:57:44 -0300

    Criação do FormCadastroDeGrupo

--- ListaDeTarefas/WindowsFormsPrincipal/FormTelaPrincipal.Designer.cs
+++ ListaDeTarefas/WindowsFormsPrincipal/FormTelaPrincipal.Designer.cs
@@ -48,7 +48,7 @@
             this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(-2, -3);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1013, 100);
+            this.panel1.Size = new System.Drawing.Size(999, 100);
             this.panel1.TabIndex = 0;
             // 
             // panel2
