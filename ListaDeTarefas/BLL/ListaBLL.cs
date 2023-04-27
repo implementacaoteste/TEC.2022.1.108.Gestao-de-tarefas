f91aa7bb6948ff653922879c5eed88f7c7c359b0
fabricio teixeira <115494324+fabriciosenai@users.noreply.github.com>
2023-04-27 15:25:28 -0300

    lista bll

--- ListaDeTarefas/BLL/ListaBLL.cs
+++ ListaDeTarefas/BLL/ListaBLL.cs
@@ -3,6 +3,7 @@ using Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,13 +15,13 @@ namespace BLL
         {
             return new ListaDAL().BuscarPorIdLista(_id);
         }
-        public void BuscarPorIdGrupo()
+        public void BuscarPorIdGrupo(int _lista)
         {
-            
+            new ListaDAL().BuscarPorIdLista(_lista);
         }
-        public void BuscarPorTodasEtapas()
+        public void BuscarPorTodasEtapas(int _lista)
         {
-
+            new ListaDAL().BuscarPorIdLista(_lista);
         }
         public void AlterarLista(int _id, string _nome)
         {
