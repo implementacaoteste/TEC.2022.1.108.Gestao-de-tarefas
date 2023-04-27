2ac45822ed063c83c0c1d8fc8d2bd2dcbc16c6e9
guilherme27-03 <115494518+guilherme27-03@users.noreply.github.com>
2023-04-27 15:19:59 -0300

    Atualização

--- ListaDeTarefas/BLL/ListaBLL.cs
+++ ListaDeTarefas/BLL/ListaBLL.cs
@@ -3,6 +3,7 @@ using Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,9 +19,9 @@ namespace BLL
         {
             
         }
-        public void BuscarPorTodasEtapas()
+        public void BuscarPorTodasEtapas(int _id)
         {
-
+            new ListaDAL().AlterarLista(_id);
         }
         public void AlterarLista(int _id, string _nome)
         {
