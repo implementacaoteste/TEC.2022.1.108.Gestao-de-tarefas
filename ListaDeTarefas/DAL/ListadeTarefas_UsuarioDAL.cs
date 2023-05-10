ff9155ff045bd65aa747af5a3cf9973ca2485797
Aarão <115494573+Aarao3007@users.noreply.github.com>
2023-05-10 16:39:31 -0300

    Criação automática das listas pt3

--- ListaDeTarefas/DAL/ListadeTarefas_UsuarioDAL.cs
+++ ListaDeTarefas/DAL/ListadeTarefas_UsuarioDAL.cs
@@ -1,6 +1,7 @@
 ﻿using Models;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,28 @@ namespace DAL
     {
         public void AdicionarListaUsuario(ListaDeTarefas_Usuario _lista_usuario)
         {
-
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
+            {
+                SqlCommand cmd = cn.CreateCommand();
+                cmd.CommandText = @"INSERT INTO ListadeTarefas_Usuario(IdListaTarefas, IdUsuario, IdPermissao) 
+                                    Values(@IdListaTarefas, @IdUsuario, @IdPermissao)";
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Parameters.AddWithValue("@IdListaTarefas", _lista_usuario.IdLista );
+                cmd.Parameters.AddWithValue("@IdUsuario", _lista_usuario.IdUsuario);
+                cmd.Parameters.AddWithValue("@IdPermissao", _lista_usuario.IdPermissao);
+                cmd.Connection = cn;
+                cn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao inserir uma relação Lista<>Usuario<>Permissao Grupo no banco de dados", ex);
+            }
+            finally
+            {
+                cn.Close();
+            }
         }
     }
 }
